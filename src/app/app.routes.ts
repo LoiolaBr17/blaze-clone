c6d8bb8976277e7b67ed7c2b9c7a5a8b22207146
Mateus Goncalves Loiola <mateustaua16@gmail.com>
2024-12-10 22:28:10 -0300

    feat: configurando o projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DoubleComponent } from './pages/double/double.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: DoubleComponent
+    }
+];
---
+++ src/app/pages/double/double.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-double',
+  imports: [],
+  templateUrl: './double.component.html',
+  styleUrl: './double.component.scss'
+})
+export class DoubleComponent {
+
+}
