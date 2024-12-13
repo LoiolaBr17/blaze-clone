3641b454b48b43cae4a0c3dcbae92b2c150d1815
Mateus Goncalves Loiola <mateustaua16@gmail.com>
2024-12-13 00:18:35 -0300

    feat: criando menu expansion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MatExpansionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
